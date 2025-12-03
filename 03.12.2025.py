8485d39d20b921a30263463e53289120fb34a740
Lobarxon <qodirovjavohir0199@gmail.com>
2025-12-03 17:50:52 +0300

    Create 03.12.2025.py

---
+++ 03.12.2025.py
@@ -0,0 +1,19 @@
+# def square(number):
+#     return number * number
+#
+# def cube(number):
+#     return number * square(number)
+#
+# print("Square of 2 is", square(2))
+# print("Cube of 3 is", cube(3))
+
+
+
+# def salomlashish(ism,fam, ot):
+#     return f"Assalomu aleykum {ism} {fam} {ot}, jamoamizga xush kelibsiz!"
+#
+# ism = input("Ismingizni kirting:")
+# fam = input("Familiyangizni kirting:")
+# ot = input("Sharifingizni kirting:")
+#
+# print(salomlashish(ism, fam, ot))
